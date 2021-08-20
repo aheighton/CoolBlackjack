0cc2639e041eab2c44a29ed1adc093bb390f1173
Alex Heighton <heiga002@hotmail.co.uk>
2021-08-20 11:10:46 +0100

    Created card class

---
+++ projectFile/blackJack/src/com/aheighton/Card.java
@@ -0,0 +1,52 @@
+package com.aheighton;
+
+import static java.lang.Integer.parseInt;
+
+public class Card
+{
+	private String value;
+	private char suit;
+
+	public Card(String value, char suit)
+	{
+		setValue(value);
+		setSuit(suit);
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+
+	public char getSuit()
+	{
+		return suit;
+	}
+
+	public void setSuit(char suit)
+	{
+		this.suit = suit;
+	}
+
+	public char getColour()
+	{
+		if (getSuit() == 'S' || getSuit() == 'C') return 'B';
+		else return 'R';
+	}
+
+	public int getPoints()
+	{
+		try
+		{
+			return parseInt(getValue());
+		} catch (Exception e)
+		{
+			return 10;
+		}
+	}
+}
